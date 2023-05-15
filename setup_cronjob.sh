c234a7fd264a4c33794a795603164a231f9c1fe1
sebiiop <102870389+sebiiop@users.noreply.github.com>
2023-05-15 18:48:07 +0200

    fixed problem in installation script

--- setup_cronjob.sh
+++ setup_cronjob.sh
@@ -6,8 +6,8 @@ path=$(pwd)
 
 file1="${path}/Temperature_Humidity_Server_and_Sensor/src/temp_hum_server.py"
 file2="${path}/Temperature_Humidity_Server_and_Sensor/src/request_esp8266.py"
-#file3="${path}/Temperature_Humidity_Server_and_Sensor/src/send_ip_to_esp8266.py"
-#file4="${path}/Temperature_Humidity_Server_and_Sensor/src/dynamic_df_plot.py"
+file3="${path}/Temperature_Humidity_Server_and_Sensor/src/send_ip_to_esp8266.py"
+file4="${path}/Temperature_Humidity_Server_and_Sensor/src/dynamic_df_plot.py"
 
 #setup crontab
 #write out current crontab
@@ -15,8 +15,8 @@ crontab -l > mycron
 #echo new cron into cron file
 echo "@reboot sleep 20 && /usr/bin/python3 ${file1}  >> /home/pi/server_log.txt" >> mycron
 echo "@reboot sleep 30 && /usr/bin/python3 ${file2}  >> /home/pi/request_log.txt" >> mycron
-#echo "*/3 * * * * /usr/bin/python3 ${file3}  >> /home/pi/send_ip_log.txt" >> mycron
-#echo "*/3 * * * * /usr/bin/python3 ${file4}  >> /home/pi/dynamic_df_plot.txt" >> mycron
+echo "*/2 * * * * /usr/bin/python3 ${file3}  >> /home/pi/send_ip_log.txt" >> mycron
+echo "*/2 * * * * /usr/bin/python3 ${file4}  >> /home/pi/dynamic_df_plot.txt" >> mycron
 
 sleep 2
 
